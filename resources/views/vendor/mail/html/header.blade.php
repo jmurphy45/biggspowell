6ab0dd642a6655b747ec64b7445575fcdca3e1fc
Brad Johnson <bjohnson@epgins.com>
2020-04-07 09:28:49 -0500

    email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}">
-    <img src="{{ URL::asset('/images/email-header.jpg') }}" alt="Biggs Powell" width="380" width="210">
+    <img src="{{ URL::asset('/images/email-header.jpg') }}" alt="Biggs Powell" width="280" width="155">
 {{--{{ $slot }}--}}
 </a>
 </td>
