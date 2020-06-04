cd0c1eaec92ce308849304d83372a4f285329ee3
Brad Johnson <bjohnson@epgins.com>
2020-06-04 12:10:48 -0500

    email sig

---
+++ resources/views/logos.blade.php
@@ -0,0 +1,9 @@
+<img src="{{ URL::asset('/images/logo-01.jpg') }}" alt="" width="200" height="31">
+
+<br>
+
+<img src="{{ URL::asset('/images/logo-02.jpg') }}" alt="" width="200" height="113">
+
+<br>
+
+<img src="{{ URL::asset('/images/logo-03.jpg') }}" alt="" width="100" height="100">
