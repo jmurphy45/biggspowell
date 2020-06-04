ddc78ae1b69daafc14d8f898ef2d769473ec4613
Brad Johnson <bjohnson@epgins.com>
2020-06-04 12:52:47 -0500

    email sig

---
+++ resources/views/layouts/logos.blade.php
@@ -0,0 +1,19 @@
+
+<html>
+    <head>
+        <title>Logos - @yield('title')</title>
+    </head>
+    <body>
+        <div class="container">
+            <img src="{{ URL::asset('/images/logo-01.jpg') }}" alt="" width="200" height="31">
+
+			<br><br><br>
+
+			<img src="{{ URL::asset('/images/logo-02.jpg') }}" alt="" width="200" height="113">
+
+			<br><br><br>
+
+			<img src="{{ URL::asset('/images/logo-03.jpg') }}" alt="" width="100" height="100">
+        </div>
+    </body>
+</html>
